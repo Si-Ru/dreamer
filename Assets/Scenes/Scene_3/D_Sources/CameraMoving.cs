d6f1aa2d7533febda744967a9593502983d06194
jack0159 <eorhr1998@naver.com>
2022-11-13 16:09:08 +0900

    카메라 시야 각도 범위, 툴바 버튼 추가
    카메라 각도 제한
    아이템 및 설정 창 버튼 활성화

--- Assets/Scenes/Scene_3/D_Sources/CameraMoving.cs
+++ Assets/Scenes/Scene_3/D_Sources/CameraMoving.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 
 public class CameraMoving : MonoBehaviour
 {
-    private float cameraDragspeed = 3.5f;
-    private float cameraMoveSpeed = 0.01f;
+    private float cameraDragspeed = -3.5f;
+    private float cameraMoveSpeed = 0.1f;
 
     private static bool draggable;
     private static Vector3 targetCameraPosition;
     private static Vector3 targetCameraRotation;
     private static Vector3 originCameraPosition;
     private static Vector3 originCameraRotation;
+
+    private static Vector3 temp;
+
+
+    private float x;
+    private float y;
+    private Vector3 rotateValue;
 
     public static void fixCameraLoc(Vector3 targetCameraPos, Vector3 targetCameraRot)
     {
@@ -46,8 +53,21 @@ public class CameraMoving : MonoBehaviour
     {
         if (draggable && checkCameraLoc() && Input.GetMouseButton(0))
         {
-            Camera.main.transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X") * cameraDragspeed, 0), Space.World);
+            
+            y = Input.GetAxis("Mouse X");
+            x = Input.GetAxis("Mouse Y");
+            rotateValue = new Vector3(x * cameraDragspeed, y * -cameraDragspeed, 0);
+
+            temp = transform.eulerAngles - rotateValue;
+            temp[0] = Mathf.Clamp(temp[0], 1, 28);
+            temp[1] = Mathf.Clamp(temp[1], 14, 88);
+
+            transform.eulerAngles = temp;
+
+            
+            //Camera.main.transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X") * cameraDragspeed, 0), Space.World);
             originCameraRotation = Camera.main.transform.eulerAngles;
+            Debug.Log(originCameraRotation);
         }
         else if (!draggable || !checkCameraLoc())
         {
--- Assets/Scenes/Scene_3/D_Sources/ShelfClickEvent.cs
+++ Assets/Scenes/Scene_3/D_Sources/ShelfClickEvent.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ShelfClickEvent : MonoBehaviour
 {
     private bool st;
-    private float speed = 0.01f;
+    private float speed = 0.1f;
     private Vector3 direction = new Vector3(-4.0f, 0.0f, 0.0f);
     private Vector3 objectLocation;
     private Vector3 defaultLocation;
